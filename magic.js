c3445cc995a71acc368eabac6127169281844908
Tin <bachmach24k@gmail.com>
2022-03-21 22:17:06 -0500

    minor tweaks

--- magic.js
+++ magic.js
@@ -12,18 +12,24 @@ let render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 1260,
-    height: 520,
-    wireframes: false
+    width: 650,
+    height: 500,
+    wireframes: true,
+    showVelocity: true,
+    
   }
 });
-
 
 // create two boxes and a ground
 let boxA = Bodies.rectangle(100,400,40,40);
 let boxB = Bodies.rectangle(450,400,40,40);
-let ground = Bodies.rectangle(600, 500, 1560, 60, {isStatic: true});
-let ceil = Bodies.rectangle(600, 20, 1560, 60, {isStatic: true});
+let ground = Bodies.rectangle(200, 600, 1560, 60, {isStatic: true});
+let ceil = Bodies.rectangle(600, 0, 1560, 60, {isStatic: true});
+let lWall = Bodies.rectangle(0, 600, 60, 1560, {isStatic: true});
+let rWall = Bodies.rectangle(800, 500, 60, 1560, {isStatic: true});
+let stack = Matter.Composites.stack(2,100,9,4,50,50, function(x,y) {
+  return Bodies.circle(300,300,15);
+});
 
 // constraints
 let mouse = Matter.Mouse.create(render.canvas);
@@ -36,7 +42,7 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 render.mouse = mouse;
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground, ceil, mouseConstraint]);
+Composite.add(engine.world, [boxA, boxB, ground, ceil, lWall, rWall,  mouseConstraint, stack]);
 
 // run the renderer
 Render.run(render);
@@ -46,4 +52,11 @@ let runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+Render.lookAt(render, {
+  min: { x: 0, y: 0 },
+  max: { x: 800, y: 600 }
+});
+
+
 
